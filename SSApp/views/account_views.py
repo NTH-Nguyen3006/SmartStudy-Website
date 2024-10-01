df8aa85590b31ef39757ee6a78ed92e2e06d474f
Nguyen3006-IT <nthn300607@gmail.com>
2024-10-01 22:08:09 +0700

    01/10/2024

--- SSApp/views/account_views.py
+++ SSApp/views/account_views.py
@@ -10,9 +10,24 @@ from django.middleware.csrf import get_token, _unmask_cipher_token
 from django.utils.crypto import constant_time_compare
 from django.core.cache import cache
 
-from requests import Response
+from rest_framework.response import Response
 from rest_framework import status
 
+
+MAIL_OTPCODE_MESSAGE = """
+Xin gửi lời chào đến người dùng SmartStudy Website!
+Hiện tại bạn đang xác thực bạn là người dùng trên website. Bạn đã đăng ký với các thông tin sau:
+- Tên đăng nhập: {username}.
+- Email của bạn : {email}.
+- Họ: {last_name}.
+- Tên: {first_name}.
+
+Đây là mã xác thực của bạn. {otpCode}
+Vui lòng quay lại website để nhập mã vào để xác thực.
+
+Xin trân thành cảm ơn bạn đã ghé thăm và trải nghiệm website lần đầu tiên.
+Chúc bạn có buổi trải nghiệm tốt.
+"""
 
 def signIn(req: HttpRequest):
     if (req.method == "GET") and (req.user.is_authenticated):
@@ -42,17 +57,85 @@ def signIn(req: HttpRequest):
 
 
 def signUp(req: HttpRequest):
+    if req.body: 
+        print(req.body)
+        data = json.loads(req.body.decode("utf-8"))
+        if not data:
+            return Response("Internal server errror", 
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        username = data.get("username")
+        email = data.get("email")
+        password = data.get("password")
+        verify_password = data.get("verify_password")
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse(data={
+                'messages': 'Tên này đã được tồn tại',
+                'error_input': 'username'
+            }, status=400)
+
+        if email and User.objects.filter(email=email).exists():
+            return JsonResponse(data={
+                'messages': 'Tài khoản email này đã được đăng ký',
+                'error_input': 'email',
+            }, status=400)
+        
+        if password != verify_password:
+            return JsonResponse(data={
+                'messages': 'Mật khẩu xác nhận không trùng khớp',
+                'error_input': 'verify_password',
+            }, status=400)
+        
+        if len(password) < 8 or len(password) > 20 :
+            return JsonResponse(data={
+                'messages': 'Mật khẩu chỉ được nằm trong phạm vi 8 đến 20 kí tự',
+                'error_input': 'password',
+            }, status=400)
+
+        if len(data) == 3:
+            user_data = User.objects.get(username=req.user.get_username())
+            user_data.username = username
+            user_data.set_password(password)
+            user_data.save()
+            
+        elif len(data) == 6:
+            otpCode = random.randint(123456, 987654)
+            cache.set(key=str(otpCode), value=data, timeout=60*3+1)
+
+            message = MAIL_OTPCODE_MESSAGE.format(
+                username=username, email=email, last_name=last_name,
+                first_name=first_name, otpCode=otpCode
+            )
+            print('send email.')
+            send_mail(
+                subject="Xác thực thông tin người dùng từ SmartStudy Website",
+                message=message,
+                from_email="smartstudy2023edu@gmail.com",
+                recipient_list=["nthn300607@gmail.com"],
+                fail_silently=False,
+            )
+
+            return JsonResponse(data={
+                "redirect": "otpcode",
+            }, status=status.HTTP_100_CONTINUE)
+            
+        return redirect("home")
+
     if req.method == "GET":
         print(req.user)
-        last_codeAuthenticate = cache.get("123456")
-        print(last_codeAuthenticate)
-        if last_codeAuthenticate:
-            print(timezone.now() - last_codeAuthenticate < timezone.timedelta(hours=1))
         if not req.user.is_anonymous:
             # nếu người dùng đã set mật khẩu
             if req.user.has_usable_password():
-                return redirect("/")
-        
+                return redirect("home")
+            
+        last_codeAuthenticate = cache.get("123456")
+        if last_codeAuthenticate:
+            print(last_codeAuthenticate)
+            print(timezone.now() - last_codeAuthenticate < timezone.timedelta(minutes=3))
+
         # Người dùng xác nhận xác thực vào được vào DB
         if req.GET.get("code"): 
             code = req.GET.get("code")
@@ -78,80 +161,13 @@ def signUp(req: HttpRequest):
     
 
     if req.method == "POST":
-        print(req.body)
-        data = json.loads(req.body.decode("utf-8"))
-        if not data:
-            return Response("Internal server errror", 
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-        username = data.get("username")
-        email = data.get("email")
-        password = data.get("password")
-        verify_password = data.get("verify_password")
-        first_name = data.get("first_name")
-        last_name = data.get("last_name")
-
-        if User.objects.filter(username=username).exists():
-            # messages.error(req, "Tên tài khoản này đã tồn tại!")
-            return JsonResponse(data={
-                'messages': 'Tên này đã được tồn tại',
-                'error_input': 'username'
-            }, status=400)
-
-        if email and User.objects.filter(email=email).exists():
-            # messages.error(req, "Tài khoản email này đã tồn tại.")
-            return JsonResponse(data={
-                'messages': 'Tài khoản email này đã được đăng ký',
-                'error_input': 'email',
-            }, status=400)
-        
-        if password != verify_password:
-            return JsonResponse(data={
-                'messages': 'Mật khẩu xác nhận không trùng khớp',
-                'error_input': 'verify_password',
-            }, status=400)
-        
-        if len(password) < 8 or len(password) > 20 :
-            return JsonResponse(data={
-                'messages': 'Mật khẩu chỉ được nằm trong phạm vi 8 đến 20 kí tự',
-                'error_input': 'password',
-            }, status=400)
-
-        if len(data) == 3:
-            user_data = User.objects.get(username=req.user.get_username())
-            user_data.username = username
-            user_data.set_password(password)
-            user_data.save()
+        if req.POST.get("code"):
+            lastCode = cache.get(key=req.POST.get("code"))
             
-        elif len(data) == 6:
-            encode = base64.b64encode(req.body).decode('utf-8')
-            urlSS = f"http://localhost:8000/register/?code={encode}"
-            codeAuthenticate = random.randint(123456, 987654)
-            cache.set(f"{codeAuthenticate}", timezone.now(), 60*3)
 
-            message = f"""
-Xin gửi lời chào đến người dùng SmartStudy Website!
-Hiện tại bạn đang xác thực bạn là người dùng trên website. Bạn đã đăng ký với các thông tin sau:
-- Tên đăng nhập: {username}
-- Email của bạn : {email}
-- Họ: {last_name}
-- Tên: {first_name}
-Xin vui lòng ấn vào url để xác thực: {urlSS}.
-Đây là mã xác thực của bạn. {codeAuthenticate}
+            return redirect("home")
 
-Xin trân thành cảm ơn bạn đã ghé thăm và trải nghiệm website lần đầu tiên.
-Chúc bạn có buổi trải nghiệm tốt.
-"""
-            send_mail(
-                subject="Xác thực thông tin người dùng từ SmartStudy Website",
-                message=message,
-                from_email="smartstudy2023edu@gmail.com",
-                recipient_list=["nthn300607@gmail.com"],
-                fail_silently=False,
-            )
-
-        return redirect("register")
-    # return render(req, "sign_up.html")
+        return redirect("home")
 
 
 def logout_user(req):
